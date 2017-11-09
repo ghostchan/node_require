7a2500479aa40c17f1cb64bd202e90ab6e86c1e1
zhi.chen <zhi.chen@ucarinc.com>
2017-11-10 00:31:54 +0800

    自己实现require

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function $require(id) {
+  const fs = require("fs");
+  const path = require("path");
+  const filename = path.join(__dirname, id);
+  const dirname = path.dirname(filename);
+
+  let code = fs.readFileSync(filename, "utf8");
+  let module = { id: filename, exports: {} };
+  let exports = module.exports;
+
+  code = `
+    (function($require,module,exports,__dirname,__filename){
+        ${code}
+    })($require,module,exports,dirname,filename);`;
+  eval(code);
+  return module.exports;
+}
+
+var m1 = $require("./module1.js");
+m1.a.say();
+m1.b.say();
